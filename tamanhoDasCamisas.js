46df527fce84d8cb4f3b1c9d610f88846c5a78a0
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2021-08-11 10:27:12 -0300

    Criação do arquivo tamanhoDasCamisas

---
+++ tamanhoDasCamisas.js
@@ -0,0 +1,13 @@
+let tamanhoBr = 'PP'
+
+if (tamanhoBr == 'PP') {
+  console.log('XS')
+} else if (tamanhoBr == 'P') {
+  console.log('S')
+} else if (tamanhoBr == 'M') {
+  console.log('M')
+} else if (tamanhoBr == 'G') {
+  console.log('L')
+} else if (tamanhoBr == 'GG') {
+  console.log('XL')
+}
